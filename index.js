da93c8495c6319f409f882ebd40c6c4ece7cea65
clay hendrix <clayhendrix5@gmail.com>
2022-08-26 20:30:55 -0500

    yo

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
-let greenCharacter = document.createElement('img')
+function newImage(){
+    let greenCharacter = document.createElement('img')
 greenCharacter.src = 'assets/green-character.gif'
 greenCharacter.style.position = 'fixed'
 greenCharacter.style.left = '100px'
 greenCharacter.style.bottom = '100px'
 document.body.append(greenCharacter)
+return newImage
+}
 
+//let greenCharacter = document.createElement('img')
+//greenCharacter.src = 'assets/green-character.gif'
+//greenCharacter.style.position = 'fixed'
+//greenCharacter.style.left = '100px'
+//greenCharacter.style.bottom = '100px'
+//document.body.append(greenCharacter)
+
+let pineTree = document.createElement ('img')
+pineTree.src = 'assets/pine-tree.png'
+pineTree.style.position = 'fixed'
+pineTree.style.left = '450px'
+pineTree.style.bottom = '200px'
+document.body.append(pineTree)
+
+newImage('assets/pine-tree.png', '450px', '200px')
+newImage('assets/tree.png', '200px', '300px')
+newImage('assets/pillar.png', '350px', '100px')
+newImage('assets/crate.png', '150px', '200px')
+newImage('assets/well.png', '500px', '425px')
+
+newItem('assets.sword.png', '500px', '405px')
